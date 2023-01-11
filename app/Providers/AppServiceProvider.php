5f2566dfbb54863fc98ae45f7ed159ddfa9f48c6
AKASH-SmartSoftwareLtd <akash.smartsoftware@yahoo.com>
2023-01-11 16:25:15 +0600

    add
    add

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Models\WebsiteInformationModel;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
