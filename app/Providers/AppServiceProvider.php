3875fc1595330a0a55698fa6704e231a1b68684c
AKASH-SmartSoftwareLtd <akash.smartsoftware@yahoo.com>
2023-01-11 16:22:09 +0600

    solve
    solve

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         Paginator::useBootstrap();
         if ( !app()->runningInConsole() ){
             $WebsiteInformation = WebsiteInformationModel::find(1);
