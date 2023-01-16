977fb8e0b14893b0e6d1c32602360c52cf639d41
utsabparoi <utshabparoy@gmail.com>
2023-01-16 15:29:20 +0600

    Few Modification

--- resources/views/frontend/layout/app.blade.php
+++ resources/views/frontend/layout/app.blade.php
@@ -78,13 +78,7 @@
 {{--<script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js"></script>--}}
 <script src="{{ asset('frontend/js/lottie-player.js') }}"></script>
 
-{{-- <script src="{{ asset('/backend/js/jquery-ui.min.js') }}"></script>
-    <script type="text/javascript">
-        $(document).ready(function() {
-          $( "#datepicker" ).datepicker({ dateFormat: 'DD-Mon-YYYY' });
-        } );
-    </script>
-<script> --}}
+<script>
     jQuery(document).ready(function() {
         $(".dropdown").hover(
             function() {
