b16d4b7b524fd1bffd0fa4e555d3b02ab75c15c6
utsabparoi <utshabparoy@gmail.com>
2023-01-21 17:30:35 +0600

    HealthPackageFacility Seeder Modified

--- database/seeders/HealthPackageFacilitySeeder.php
+++ database/seeders/HealthPackageFacilitySeeder.php
@@ -15,19 +15,19 @@ class HealthPackageFacilitySeeder extends Seeder
     public function run()
     {
         $health_facilities = array(
-            array('id' => '1','name' => 'Free Consultation', 'status' => 1),
-            array('id' => '2','name' => 'Fast & Accurate Report', 'status' => 1),
-            array('id' => '3','name' => 'Better Health Care', 'status' => 1),
-            array('id' => '4','name' => 'Complimentary Breakfast', 'status' => 1),
-            array('id' => '5','name' => 'Dedicated Customer Service', 'status' => 1),
-            array('id' => '6','name' => 'Consultation with Physicion', 'status' => 1),
-            array('id' => '7','name' => 'On Time Sample Collection', 'status' => 1),
-            array('id' => '8','name' => 'Diet Consultancy', 'status' => 1),
-            array('id' => '9','name' => 'On Time Sample Collection', 'status' => 1),
-            array('id' => '10','name' => 'Fast & Accurate Report', 'status' => 1),
-            array('id' => '11','name' => 'Online report view', 'status' => 1),
-            array('id' => '12','name' => 'Skilled Phlebotomist', 'status' => 1),
-            array('id' => '13','name' => 'Covid-19 negetive sample callector', 'status' => 1),
+            array('id' => '1','name' => 'Free Consultation', 'pkg_category' => 'Regular Health Screening', 'status' => 1),
+            array('id' => '2','name' => 'Fast & Accurate Report', 'pkg_category' => 'Regular Health Screening', 'status' => 1),
+            array('id' => '3','name' => 'Better Health Care', 'pkg_category' => 'Regular Health Screening', 'status' => 1),
+            array('id' => '4','name' => 'Complimentary Breakfast', 'pkg_category' => 'Regular Health Screening', 'status' => 1),
+            array('id' => '5','name' => 'Dedicated Customer Service', 'pkg_category' => 'Regular Health Screening', 'status' => 1),
+            array('id' => '6','name' => 'Consultation with Physicion', 'pkg_category' => 'Regular Health Screening', 'status' => 1),
+            array('id' => '7','name' => 'On Time Sample Collection', 'pkg_category' => 'Regular Health Screening', 'status' => 1),
+            array('id' => '8','name' => 'Diet Consultancy', 'pkg_category' => 'Regular Health Screening', 'status' => 1),
+            array('id' => '9','name' => 'On Time Sample Collection', 'pkg_category' => 'Home Health Screening', 'status' => 1),
+            array('id' => '10','name' => 'Fast & Accurate Report', 'pkg_category' => 'Home Health Screening', 'status' => 1),
+            array('id' => '11','name' => 'Online report view', 'pkg_category' => 'Home Health Screening', 'status' => 1),
+            array('id' => '12','name' => 'Skilled Phlebotomist', 'pkg_category' => 'Home Health Screening', 'status' => 1),
+            array('id' => '13','name' => 'Covid-19 negetive sample callector', 'pkg_category' => 'Home Health Screening', 'status' => 1),
 
         );
 
